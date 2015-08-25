2ccf9b9415c14bb5e91106a72295476202b86acb
inf3rno <laszlo.janszky@gmail.com>
2015-08-25 08:17:22 +0200

    Gitignore file was missing, I had to add it to ignore webstorm files. The procedural code was refactored to basic oo. It requires further refactoring, but the current form is good enough to commit. The stocks.list file was removed, it's content was moved to a textview in the settings schema. It will require further code to don't update by every keystroke. The stylesheet.css was cleared from unused code. Put a huge part of the code into a try-catch block, so it won't crash by most of the errors.

--- README.md
+++ README.md
@@ -7,15 +7,36 @@
 This repository contains the directory "yahoostocks@fthuin". You can
 download it and move it in ~/.local/share/cinnamon/desklets/
 
-This will shows (by default every 10 minutes) updated data from Yahoo
+This will show (by default every 10 minutes) updated data from Yahoo
 Finance on your cinnamon desktop. You can choose which quotes you want to display
-in yahoostocks@fthuin/stocks.list by removing or adding quotes symbols.
+in the settings by removing or adding quotes symbols.
 
 This desklet was inspired by the stocks applet from adonut
 (http://cinnamon-spices.linuxmint.com/applets/view/187) but it retrieved
 data from Google Finance API which is deprecated.
 
 Tested with Linux Mint 17.2 / Cinnamon 2.6.13 (Fully functional)
+
+## Release logs
+
+### 0.2 - Aug 25, 2015
+
+Features:
+
+ - reading symbols from the settings instead of the stocks.list file
+ - slightly improved error handling, it prints error messages into the console instead of crashing
+
+### 0.1.0 - Jul 15, 2015
+
+Features:
+
+ - reading width, height and delay from the settings
+ - resize main box by the changes of width and height
+ - reading symbols from the stocks.list file
+ - loading stock quotes from yahoo api filtered by the symbols
+ - displaying stock quote data in a vertically scrollable table in the main box
+ - update stock quote data regularly based on the delay minutes
+
 
 ## This program is a free software:
 
--- yahoostocks@fthuin/desklet.js
+++ yahoostocks@fthuin/desklet.js
@@ -1,9 +1,3 @@
-/* Author : Florian Thuin */
-
-/***************
- * Libs imports
- ***************/
-
 const Desklet = imports.ui.desklet; // cinnamon desklet user interface
 const St = imports.gi.St; // Shell toolkit library from GNOME
 const Gio = imports.gi.Gio; // URL-IO-Operations
@@ -11,239 +5,227 @@ const GLib = imports.gi.GLib; // Files operations
 const Gtk = imports.gi.Gtk; // Gtk library (policies for scrollview)
 const Mainloop = imports.mainloop; // For repeated updating
 const Lang = imports.lang; // Binding desklet to mainloop function
-const Settings = imports.ui.settings; // Load settings-schema.json file
+const Settings = imports.ui.settings; // Settings loader based on settings-schema.json file
 
-/************
- * Variables
- ************/
-
-var stocksFilePath = '/stocks.list';
+var console = global; // So we can use console.log
 var dirPath = 'yahoostocks@fthuin';
 var deskletDir = GLib.get_home_dir() + "/.local/share/cinnamon/desklets/" + dirPath;
-var comparray = read_file(); /* Companies array */
-var mainBox; // BoxLayout integrating whole UI
 
-/************
- * Functions
- ************/
 
-/* Desklet constructor */
-function MyDesklet(metadata, desklet_id) {
-    this._init(metadata, desklet_id);
+function main(metadata, id) {
+    return new StockQuoteDesklet(metadata, id);
 }
 
-/* Prototype */
-MyDesklet.prototype = {
+function StockQuoteDesklet(metadata, id) {
+    Desklet.Desklet.prototype._init.call(this, metadata, id);
+    this.init(metadata, id);
+}
+
+StockQuoteDesklet.prototype = {
     __proto__: Desklet.Desklet.prototype,
-
-    /* Initialisation of the desklet */
-    _init: function(metadata, desklet_id) {
-        Desklet.Desklet.prototype._init.call(this, metadata, desklet_id);
-
-        /* Load settings-schema.json file */
-        this.settings = new Settings.DeskletSettings(this, this.metadata['uuid'], desklet_id);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "height", "height", this._onDisplayChanged, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "width", "width", this._onDisplayChanged, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "delay", "delay", this._onSettingsChanged, null);
-
-        this.update_stocks();
+    init: function (metadata, id) {
+        this.metadata = metadata;
+        this.id = id;
+        this.stockReader = new YahooServiceBasedStockQuoteReader();
+        this.loadSettings();
+        this.onUpdate();
     },
-
-    _onDisplayChanged : function() {
-        mainBox.set_size(this.width, this.height);
+    loadSettings: function () {
+        this.settings = new Settings.DeskletSettings(this, this.metadata['uuid'], this.id);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "height", "height", this.onDisplayChanged, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "width", "width", this.onDisplayChanged, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "delayMinutes", "delayMinutes", this.onSettingsChanged, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "companySymbols", "companySymbolsText", this.onSettingsChanged, null);
     },
-
-     _onSettingsChanged : function() {
-        mainBox.destroy_all_children();
-        mainBox.destroy();
-        Mainloop.source_remove(this.mainloop);
-        this.update_stocks();
-     },
-
-     update_stocks : function() {
-        /* Render the values onto the desklet */
-        // Main box (stores everything)
-        mainBox = new St.BoxLayout({
-                vertical : true,
-                width : this.width,
-                height : this.height,
-                style_class: "stocks-reader"});
-        // ScrollView will be put in the main box
-        this._view = new St.ScrollView();
-        this._view.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC);
-
-        // Table in which we store every stocks
-        this._stocksTable = new St.Table({homogeneous: false});
-        // Box in which we will store the table
-        this._stocksBox = new St.BoxLayout({
-                vertical : true,
-        });
-
-        var quotes = get_stocks();
-        for (var i = 0, l = quotes.length ; i < l ; i++) {
-            var myStock = quotes[i];
-            var cur = "";
-            var icon_path = "";
-
-            cur = get_currency_symbol(myStock['Currency']);
-
-            /* Set the change icon, last char is % so must be ignored : */
-            if (parseFloat(myStock['PercentChange'].slice(0,-1)) > 0) icon_path = "/icons/up.svg";
-            else if (parseFloat(myStock['PercentChange'].slice(0,-1)) < 0) icon_path = "/icons/down.svg";
-            else if (parseFloat(myStock['PercentChange'].slice(0, -1)) == 0) icon_path = "/icons/eq.svg";
-            // First thing will be the icon of the result
-            var binIcon = new St.Bin( {height : "20px", width : "20px" });
-            var file = Gio.file_new_for_path(deskletDir + ''+ icon_path);
-            var icon_uri = file.get_uri();
-            var image = St.TextureCache.get_default().load_uri_async(icon_uri, -1, -1);
-                image.set_size(20,20);
-                binIcon.set_child(image);
-
-                this._stocksTable.add(binIcon, {
-                        row : i,
-                        col : 0,
-                        style_class : "stocks-table-item"
-                });
-
-                // Second column is the name of the company
-                var stockName = new St.Label( {
-                        text : myStock['Name'],
-                        style_class : "stocks-label" });
-
-                this._stocksTable.add(stockName, {
-                        row : i,
-                        col : 1,
-                        style_class : "stocks-table-item"
-                });
-
-                // Third thing is the stock symbol
-                var stockSymbol = new St.Label({
-                        text : myStock['symbol'],
-                        style_class : "stocks-label"
-                });
-                
-                this._stocksTable.add(stockSymbol, {
-                        row : i,
-                        col : 2,
-                        style_class : "stocks-table-item"
-                });
-
-                var stockPrice = new St.Label( {
-                        text : cur + '' + myStock['LastTradePriceOnly'],
-                        style_class : "stocks-label"
-                });
-
-                this._stocksTable.add(stockPrice, {
-                        row : i,
-                        col : 3,
-                        style_class : "stocks-table-item"
-                });
-
-                // Fifth thing is the percent change
-                var stockPerChange = new St.Label( {
-                        text : myStock['PercentChange'],
-                        style_class : "stocks-label"
-                });
-
-                this._stocksTable.add(stockPerChange, {
-                        row : i,
-                        col : 4,
-                        style_class : "stocks-table-item"
-                });
-
-            }
-
-            this._stocksBox.add_actor(this._stocksTable);
-            this._view.add_actor(this._stocksBox);
-
-            mainBox.add(this._view, {expand: true});
-
-            this.setContent(mainBox);
-
-            /* Update every X milliseconds */
-            this.mainloop = Mainloop.timeout_add(this.delay * 60 * 1000, Lang.bind(this, this.update_stocks));
-     }
-}
-
-/* Get all the stocks at once from Yahoo */
-function get_stocks() {
-        var allcompanies = '';
-        var i = 0;
-        for (var limit = comparray.length - 1 ; i < limit ; i++) {
-            allcompanies += comparray[i] + '%22%2C%22';
-        }
-        allcompanies += '' + comparray[i];
-        
-        /* Retrieving data from Yahoo */
-        var urlcatch = Gio.file_new_for_uri('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22'+ allcompanies +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=');
-        var loaded = false;
-        var quotes = [];
-
+    onDisplayChanged: function () {
+        this.resize();
+    },
+    onSettingsChanged: function () {
+        this.unrender();
+        this.removeUpdateTimer();
+        this.onUpdate();
+    },
+    onUpdate: function () {
+        var companySymbols = this.companySymbolsText.split("\n");
         try {
-            loaded = urlcatch.load_contents(null)[0];
-        } catch (err) {
-        return "Invalid Stock?: "+company;
-    }
-
-    if (loaded == true) {
-        // Load the complete result from http-GET into str
-        var str = (urlcatch.load_contents(null)[1]);
-
-        /* Now we create a string array */
-        var valuelist = (str).toString();
-        var jsonObject = JSON.parse(valuelist);
-
-        var cnt = jsonObject['query']['count']; // Number of retrieved quotes
-        if (cnt == 1) {
-                quotes[0] = jsonObject['query']['results']['quote'];
+            var stockQuotes = this.stockReader.getStockQuotes(companySymbols);
+            this.render(stockQuotes);
+            this.setUpdateTimer();
         }
-        else {
-            for (var i = 0 ; i < cnt ; i++) {
-                quotes[i] = jsonObject['query']['results']['quote'][i];
-            }
+        catch (err) {
+            this.onError(companySymbols, err);
         }
-    } else {
-        global.log(dirPath + " - Cannot find anything for symbol:  " +symbol);
+    },
+    onError: function (companySymbols, err) {
+        console.log("Cannot get stock quotes for company symbols: " + companySymbols.join(","));
+        console.log("The following error occured: " + err);
+        console.log("Shutting down...");
+    },
+    render: function (stockQuotes) {
+        var table = new StockQuotesTable();
+        table.render(stockQuotes);
+
+        var tableContainer = new St.BoxLayout({
+            vertical: true
+        });
+        tableContainer.add_actor(table.el);
+
+        var scrollView = new St.ScrollView();
+        scrollView.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC);
+        scrollView.add_actor(tableContainer);
+
+        this.mainBox = new St.BoxLayout({
+            vertical: true,
+            width: this.width,
+            height: this.height,
+            style_class: "stocks-reader"
+        });
+        this.mainBox.add(scrollView, {expand: true});
+        this.setContent(this.mainBox);
+    },
+    unrender: function () {
+        this.mainBox.destroy_all_children();
+        this.mainBox.destroy();
+    },
+    resize: function () {
+        this.mainBox.set_size(this.width, this.height);
+    },
+    setUpdateTimer: function () {
+        this.updateLoop = Mainloop.timeout_add(this.delayMinutes * 60 * 1000, Lang.bind(this, this.onUpdate));
+    },
+    removeUpdateTimer: function () {
+        Mainloop.source_remove(this.updateLoop);
     }
-    return quotes;
-}
+};
 
-/* Takes in input the abrev name of a currency, output the related
- * symbol */
-function get_currency_symbol(curName) {
-    var cursymb;
-    switch (curName) {
-        case "USD" : cursymb = "$";
-            break;
-        case "EUR" : cursymb = "\u20AC";
-            break;
-        case "JPY" : cursymb = "\u00A5";
-            break;
-        case "GBP" : cursymb = "\u00A3";
-            break;
-        case "INR" : cursymb = "\u20A8";
-        default : cursymb = curName;
+var StockQuotesTable = function () {
+    this.el = new St.Table({homogeneous: false});
+};
+StockQuotesTable.prototype = {
+    constructor: StockQuotesTable,
+    currencyCodeToSymbolMap: {
+        USD: "$",
+        EUR: "\u20AC",
+        JPY: "\u00A5",
+        GBP: "\u00A3",
+        INR: "\u20A8"
+    },
+    render: function (stockQuotes) {
+        for (var rowIndex = 0, l = stockQuotes.length; rowIndex < l; rowIndex++)
+            this.renderTableRow(stockQuotes[rowIndex], rowIndex);
+    },
+    renderTableRow: function (stockQuote, rowIndex) {
+        var cellContents = [
+            this.createPercentChangeIcon(stockQuote),
+            this.createCompanyNameLabel(stockQuote),
+            this.createStockSymbolLabel(stockQuote),
+            this.createStockPriceLabel(stockQuote),
+            this.createPercentChangeLabel(stockQuote)
+        ];
+
+        for (var columnIndex = 0; columnIndex < cellContents.length; ++columnIndex)
+            this.el.add(cellContents[columnIndex], {
+                row: rowIndex,
+                col: columnIndex,
+                style_class: "stocks-table-item"
+            });
+    },
+    createStockSymbolLabel: function (stockQuote) {
+        return new St.Label({
+            text: stockQuote.symbol,
+            style_class: "stocks-label"
+        });
+    },
+    createStockPriceLabel: function (stockQuote) {
+        var currencyCode = stockQuote.Currency;
+        var currencySymbol = this.currencyCodeToSymbolMap[currencyCode] || currencyCode;
+        return new St.Label({
+            text: currencySymbol + '' + stockQuote.LastTradePriceOnly,
+            style_class: "stocks-label"
+        });
+    },
+    createCompanyNameLabel: function (stockQuote) {
+        return new St.Label({
+            text: stockQuote.Name,
+            style_class: "stocks-label"
+        });
+    },
+    createPercentChangeIcon: function (stockQuote) {
+        var path = "";
+        var percentChange = parseFloat(stockQuote.PercentChange.slice(0, -1));
+
+        if (percentChange > 0)
+            path = "/icons/up.svg";
+        else if (percentChange < 0)
+            path = "/icons/down.svg";
+        else if (percentChange == 0)
+            path = "/icons/eq.svg";
+        var iconFile = Gio.file_new_for_path(deskletDir + '' + path);
+
+        var uri = iconFile.get_uri();
+        var image = St.TextureCache.get_default().load_uri_async(uri, -1, -1);
+        image.set_size(20, 20);
+
+        var binIcon = new St.Bin({height: "20px", width: "20px"});
+        binIcon.set_child(image);
+        return binIcon;
+    },
+    createPercentChangeLabel: function (stockQuote) {
+        return new St.Label({
+            text: stockQuote.PercentChange,
+            style_class: "stocks-label"
+        });
     }
-    return cursymb;
+};
 
-}
 
-/* Read file containing stocks symbol line by line */
-function read_file() {
-    var file = deskletDir + '' + stocksFilePath ;
+var YahooServiceBasedStockQuoteReader = function () {
+};
 
-    if (GLib.file_test(file, GLib.FileTest.EXISTS)) {
-        var content = GLib.file_get_contents(file);
-        var stocklist = content.toString().split('\n').slice(0,-1)
-        /* Get rid of 'true,' in the first field */
-        stocklist[0] = stocklist[0].replace("true,", "");
-        return stocklist;
-    } else {
-        return ['No Companies defined in: ' + this.file];
+YahooServiceBasedStockQuoteReader.prototype = {
+    constructor: YahooServiceBasedStockQuoteReader,
+    yqlServiceUrl: "https://query.yahooapis.com/v1/public/yql",
+    yqlTemplate: "select * from yahoo.finance.quotes where symbol in (:selectedCompanyCodes)",
+    getStockQuotes: function (companySymbols) {
+        var request = this.createYQLServiceRequest(companySymbols);
+        var response = this.getYQLServiceResponse(request);
+        return this.fetchStockQuotes(response);
+    },
+    createYQLServiceRequest: function (companySymbols) {
+        var yql = this.yqlTemplate.replace(":selectedCompanyCodes", '"' + companySymbols.join('","') + '"');
+        var requestDetails = {
+            q: yql,
+            format: "json",
+            env: "store://datatables.org/alltableswithkeys",
+            callback: ""
+        };
+        var encodedRequestDetails = [];
+        for (var property in requestDetails)
+            encodedRequestDetails.push(encodeURI(property) + "=" + encodeURIComponent(requestDetails[property]));
+        return this.yqlServiceUrl + "?" + encodedRequestDetails.join("&");
+    },
+    getYQLServiceResponse: function (request) {
+        var urlcatch = Gio.file_new_for_uri(request);
+        var loaded = false,
+            content;
+        loaded = urlcatch.load_contents(null)[0];
+        if (!loaded)
+            throw new Error("Service not available?");
+        content = urlcatch.load_contents(null)[1];
+        return JSON.parse(content.toString());
+    },
+    fetchStockQuotes: function (response) {
+        var quotes = [];
+        var dataRows = response.query.results.quote;
+        var rowCount = response.query.count;
+        if (rowCount == 1)
+            dataRows = [response.query.results.quote];
+        for (var rowIndex = 0; rowIndex < rowCount; rowIndex++)
+            quotes[rowIndex] = dataRows[rowIndex];
+        return quotes;
     }
-}
+};
 
-/* Main function, called by default, returns our desklet */
-function main(metadata, desklet_id) {
-    return new MyDesklet(metadata, desklet_id);
-}
+
+
+
